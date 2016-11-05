173e36f0786e75ac9662b6e7721f2346217c0b00
Matt Mauro <matt.mauro@gmail.com>
2016-11-05 15:09:30 -0400

    tutorial finished

---
+++ Vehicles/Truck.h
@@ -0,0 +1,15 @@
+//
+//  Truck.h
+//  Vehicles
+//
+//  Created by Matthew Mauro on 2016-11-05.
+//  Copyright © 2016 Designated Nerd Software. All rights reserved.
+//
+
+#import "Vehicle.h"
+
+@interface Truck : Vehicle
+@property (nonatomic, assign) NSInteger cargoCapacityCubicFeet;
+
+
+@end
--- Vehicles/VehicleListTableViewController.m
+++ Vehicles/VehicleListTableViewController.m
@@ -11,6 +11,7 @@
 #import "VehicleDetailViewController.h"
 #import "Vehicle.h"
 #import "Motorcycle.h"
+#import "Truck.h"
 
 @interface VehicleListTableViewController ()
 @property (nonatomic, strong) NSMutableArray *vehicles;
@@ -115,6 +116,30 @@
     
     //Add it to the array
     [self.vehicles addObject:kawasaki];
+    
+    //Create a truck
+    Truck *silverado = [[Truck alloc] init];
+    silverado.brandName = @"Chevrolet";
+    silverado.modelName = @"Silverado";
+    silverado.modelYear = 2011;
+    silverado.numberOfWheels = 4;
+    silverado.cargoCapacityCubicFeet = 53;
+    silverado.powerSource = @"gas engine";
+    
+    //Add it to the array
+    [self.vehicles addObject:silverado];
+    
+    //Create another truck
+    Truck *eighteenWheeler = [[Truck alloc] init];
+    eighteenWheeler.brandName = @"Peterbilt";
+    eighteenWheeler.modelName = @"579";
+    eighteenWheeler.modelYear = 2013;
+    eighteenWheeler.numberOfWheels = 18;
+    eighteenWheeler.cargoCapacityCubicFeet = 408;
+    eighteenWheeler.powerSource = @"diesel engine";
+    
+    //Add it to the array
+    [self.vehicles addObject:eighteenWheeler];
 }
 
 #pragma mark - Table View Data Source
